9f20c7d6d2da6028e71bf648ecc3fd35a6b1ed41
Ignas Kraukšlys <ignkra1@ktu.lt>
2021-12-05 00:45:23 +0200

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(2);
+            numbers.Add(4);
+            numbers.Add(8);
+            var squaredNumbers = numbers.Select(x => x + "labas");
+
+            foreach (var item in squaredNumbers)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
